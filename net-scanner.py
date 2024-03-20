01918689159cd7da273b784806256fcb46fc93ba
Buğrahan Y <78935299+Bqrayvzdgn@users.noreply.github.com>
2024-03-20 23:20:15 +0300

    Update net-scanner.py

--- net-scanner.py
+++ net-scanner.py
@@ -10,7 +10,6 @@ ERROR = """
 ╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝
 """
 
-
 def get_user_input():
     parse_object = optparse.OptionParser()
     parse_object.add_option("-i", "--ipaddress", dest="ip_address", help="Enter IP Address")
@@ -20,14 +19,12 @@ def get_user_input():
     else:
         return user_input
 
-
 def scan_my_network(ip):
     arp_request_packet = scapy.ARP(pdst=ip)
     broadcast_packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     combined_packet = broadcast_packet / arp_request_packet
     (answered_list, unanswered_list) = scapy.srp(combined_packet, timeout=1)
     answered_list.summary()
-
 
 if __name__ == "__main__":
     user_ip_address = get_user_input()
