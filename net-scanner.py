e0200ee317078ad6aaf6cf654ddaecb166452d39
Buğrahan Y <78935299+Bqrayvzdgn@users.noreply.github.com>
2024-03-20 23:42:59 +0300

    Update net-scanner.py

--- net-scanner.py
+++ net-scanner.py
@@ -1,5 +1,5 @@
 import scapy.all as scapy
-import optparse
+import argparse
 
 class Banners:
     ERROR = """
@@ -22,24 +22,20 @@ class Banners:
 
 def get_user_input():
     print(Banners.LOGO)
-    parse_object = optparse.OptionParser()
-    parse_object.add_option("-i","--ipaddress", dest="ip_address",help="Enter IP Address")
-
-    (user_input,arguments) = parse_object.parse_args()
-
+    parser = argparse.ArgumentParser(description="This application was developed by kenxzz.", usage="python net-scanner.py -i [IP Field]", epilog="[Important] You must have sudo privileges for Net scanner to work properly.!")
+    parser.add_argument("-i","--ipaddress", dest="ip_address",help="Enter IP Address")
+    (user_input,arguments) = parser.parse_args()
     if not user_input.ip_address:
         print(Banners.ERROR)
-
     return user_input
 
 def scan_my_network(ip):
     arp_request_packet = scapy.ARP(pdst=ip)
-    #scapy.ls(scapy.ARP())
     broadcast_packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    #scapy.ls(scapy.Ether())
     combined_packet = broadcast_packet/arp_request_packet
     (answered_list,unanswered_list) = scapy.srp(combined_packet,timeout=1)
     answered_list.summary()
 
-user_ip_address = get_user_input()
-scan_my_network(user_ip_address.ip_address)
+if __name__ == "__main__":
+    user_ip_address = get_user_input()
+    scan_my_network(user_ip_address.ip_address)
