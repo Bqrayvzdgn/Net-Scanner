974b50aad8b3b5fb7e40a6db9cdb7a051a15cee7
Buğrahan Y <78935299+Bqrayvzdgn@users.noreply.github.com>
2024-03-21 00:39:38 +0300

    Update net-scanner.py

--- net-scanner.py
+++ net-scanner.py
@@ -1,18 +1,34 @@
 import scapy.all as scapy
 import optparse
 
-#1)arp_request
-#2)broadcast
-#3)response
+class Banners:
+    ERROR = """
+    ███████╗██████╗░██████╗░░█████╗░██████╗░██╗
+    ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗██║
+    █████╗░░██████╔╝██████╔╝██║░░██║██████╔╝██║
+    ██╔══╝░░██╔══██╗██╔══██╗██║░░██║██╔══██╗╚═╝
+    ███████╗██║░░██║██║░░██║╚█████╔╝██║░░██║██╗
+    ╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝
+    """
+
+    LOGO = """
+    ██████╗░░██████╗░██████╗░██████╗░███████╗██╗░░░██╗
+    ██╔══██╗██╔═══██╗██╔══██╗██╔══██╗██╔════╝██║░░░██║
+    ██████╦╝██║██╗██║██████╔╝██║░░██║█████╗░░╚██╗░██╔╝
+    ██╔══██╗╚██████╔╝██╔══██╗██║░░██║██╔══╝░░░╚████╔╝░
+    ██████╦╝░╚═██╔═╝░██║░░██║██████╔╝███████╗░░╚██╔╝░░
+    ╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝╚═════╝░╚══════╝░░░╚═╝░░░
+    """
 
 def get_user_input():
+    print(Banners.LOGO)
     parse_object = optparse.OptionParser()
     parse_object.add_option("-i","--ipaddress", dest="ip_address",help="Enter IP Address")
 
     (user_input,arguments) = parse_object.parse_args()
 
     if not user_input.ip_address:
-        print("Enter IP Address")
+        print(Banners.ERROR)
 
     return user_input
 
