0e09d9081d69811a155bfca1998611a430db2c34
Buğrahan Y <78935299+Bqrayvzdgn@users.noreply.github.com>
2024-03-20 23:32:17 +0300

    Update

--- net-scanner.py
+++ net-scanner.py
@@ -21,22 +21,24 @@ class Banners:
 """
 
 def get_user_input():
-    print(Banners.LOGO)
     parse_object = optparse.OptionParser()
-    parse_object.add_option("-i", "--ipaddress", dest="ip_address", help="Enter IP Address")
-    (user_input, arguments) = parse_object.parse_args()
+    parse_object.add_option("-i","--ipaddress", dest="ip_address",help="Enter IP Address")
+
+    (user_input,arguments) = parse_object.parse_args()
+
     if not user_input.ip_address:
-        print(Banners.ERROR)
-    else:
-        return user_input
+        print("Enter IP Address")
+
+    return user_input
 
 def scan_my_network(ip):
     arp_request_packet = scapy.ARP(pdst=ip)
+    #scapy.ls(scapy.ARP())
     broadcast_packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    combined_packet = broadcast_packet / arp_request_packet
-    (answered_list, unanswered_list) = scapy.srp(combined_packet, timeout=1)
+    #scapy.ls(scapy.Ether())
+    combined_packet = broadcast_packet/arp_request_packet
+    (answered_list,unanswered_list) = scapy.srp(combined_packet,timeout=1)
     answered_list.summary()
 
-if __name__ == "__main__":
-    user_ip_address = get_user_input()
-    scan_my_network(user_ip_address)
+user_ip_address = get_user_input()
+scan_my_network(user_ip_address.ip_address)
