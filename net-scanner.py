a64355f66d448bd1faff4eaaa103715dc1794c3f
Buğrahan Y <78935299+Bqrayvzdgn@users.noreply.github.com>
2024-03-21 13:46:55 +0300

    Update net-scanner.py

--- net-scanner.py
+++ net-scanner.py
@@ -3,24 +3,22 @@ import optparse
 
 class Banners:
     ERROR = """
-    ███████╗██████╗░██████╗░░█████╗░██████╗░██╗
-    ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗██║
-    █████╗░░██████╔╝██████╔╝██║░░██║██████╔╝██║
-    ██╔══╝░░██╔══██╗██╔══██╗██║░░██║██╔══██╗╚═╝
-    ███████╗██║░░██║██║░░██║╚█████╔╝██║░░██║██╗
-    ╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝
+    ▒█▀▀▀ ▒█▀▀█ ▒█▀▀█ ▒█▀▀▀█ ▒█▀▀█ █ 
+    ▒█▀▀▀ ▒█▄▄▀ ▒█▄▄▀ ▒█░░▒█ ▒█▄▄▀ ▀ 
+    ▒█▄▄▄ ▒█░▒█ ▒█░▒█ ▒█▄▄▄█ ▒█░▒█ ▄
+    """
+
+    QUIT = """
+    ▒█▀▀▀█ ▒█▀▀▀ ▒█▀▀▀ ▒█░░▒█ ▒█▀▀▀█ ▒█░▒█ 
+    ░▀▀▀▄▄ ▒█▀▀▀ ▒█▀▀▀ ▒█▄▄▄█ ▒█░░▒█ ▒█░▒█ 
+    ▒█▄▄▄█ ▒█▄▄▄ ▒█▄▄▄ ░░▒█░░ ▒█▄▄▄█ ░▀▄▄▀
     """
 
     LOGO = """
-    ██████╗░░██████╗░██████╗░██████╗░███████╗██╗░░░██╗
-    ██╔══██╗██╔═══██╗██╔══██╗██╔══██╗██╔════╝██║░░░██║
-    ██████╦╝██║██╗██║██████╔╝██║░░██║█████╗░░╚██╗░██╔╝
-    ██╔══██╗╚██████╔╝██╔══██╗██║░░██║██╔══╝░░░╚████╔╝░
-    ██████╦╝░╚═██╔═╝░██║░░██║██████╔╝███████╗░░╚██╔╝░░
-    ╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝╚═════╝░╚══════╝░░░╚═╝░░░
+    ▒█▀▀█ ▒█▀▀█ ▒█▀▀█ ▒█▀▀▄ ▒█▀▀▀ ▒█░░▒█ 
+    ▒█▀▀▄ ▒█░▒█ ▒█▄▄▀ ▒█░▒█ ▒█▀▀▀ ░▒█▒█░ 
+    ▒█▄▄█ ░▀▀█▄ ▒█░▒█ ▒█▄▄▀ ▒█▄▄▄ ░░▀▄▀░
     """
-    
-    SEPARATOR = "==================================================="
 
 
 def get_user_input():
@@ -37,15 +35,14 @@ def get_user_input():
 
 def scan_my_network(ip):
     arp_request_packet = scapy.ARP(pdst=ip)
-    #scapy.ls(scapy.ARP())
     broadcast_packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    #scapy.ls(scapy.Ether())
     combined_packet = broadcast_packet/arp_request_packet
     (answered_list,unanswered_list) = scapy.srp(combined_packet,timeout=1)
     answered_list.summary()
-    print(Banners.SEPARATOR)
-    unanswered_list.summary()
+    #print(Banners.SEPARATOR)
+    #unanswered_list.summary()
     
-
-user_ip_address = get_user_input()
-scan_my_network(user_ip_address.ip_address)
+if __name__ == "__main__":
+    user_ip_address = get_user_input()
+    scan_my_network(user_ip_address.ip_address)
+    print(Banners.QUIT)
